47572111bdc639fdf410148e89f5e65199a09fd0
Miguel Seca <miguel.seca@gmail.com>
2022-09-01 14:46:17 +0100

    trip service created

---
+++ src/app/services/trips.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { CONFIG } from 'src/assets/config';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import Trip from '../models/trip';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TripsService {
+  private TRIPS_URL = CONFIG.TRIPS_URL;
+
+  private token: string = localStorage.getItem('token')!.slice(1, -1);
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${this.token}`,
+    }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getAllTrips(): Observable<Trip[]> {
+    return this.http.get<Trip[]>(this.TRIPS_URL, this.httpOptions).pipe(
+      tap((_) => this.log('get trips successfull!')),
+      catchError(this.handleError<Trip[]>('getAllTrips', []))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    console.log(`Cargas Service: ${message}`);
+  }
+}
