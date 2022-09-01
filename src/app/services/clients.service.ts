0c199c37230b9840a5ba14751d436412ad66a69f
Diogo Mendes <diogomendes@upskill.pt>
2022-09-01 09:56:41 +0100

    Service content (initial commit)

--- src/app/services/clients.service.ts
+++ src/app/services/clients.service.ts
@@ -1,9 +1,61 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
+// import { catchError, Observable, of, tap } from 'rxjs';
+// import { CONFIG } from 'src/assets/config';
+// import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import Carga from '../models/Carga';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ClientsService {
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class CargasService {
+//   private cargasURL = CONFIG.cargasURL;
 
-  constructor() { }
-}
+//   private token: string = localStorage.getItem("token")!.slice(1,-1);
+
+//   httpOptions = {
+//     headers: new HttpHeaders({
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${this.token}`,
+//     }),
+//   };
+
+//   constructor(private http: HttpClient) {}
+
+//   getCargas(): Observable<Carga[]> {
+//     return this.http.get<Carga[]>(this.cargasURL, this.httpOptions).pipe(
+//       tap((_) => this.log('get cargas successfull!')),
+//       catchError(this.handleError<Carga[]>('getCargas', []))
+//       );
+//     }
+
+//     updateCarga(carga: Carga): Observable<any> {
+//       return this.http.put(this.cargasURL, carga, this.httpOptions).pipe(
+//         tap((_) => this.log(`updated carga AWB=${carga.AWB}`)),
+//       catchError(this.handleError<any>('updateCarga'))
+//     );
+//   }
+
+//   addCarga(carga: Carga): Observable<any> {
+//     return this.http.post<Carga>(this.cargasURL, carga, this.httpOptions).pipe(
+//       tap(() => this.log(`new carga added AWB=${carga.AWB}`)),
+//       catchError(this.handleError<any>('addCarga'))
+//     );
+//   }
+
+//   private handleError<T>(operation = 'operation', result?: T) {
+//     return (error: any): Observable<T> => {
+//       // TODO: send the error to remote logging infrastructure
+//       console.error(error); // log to console instead
+
+//       // TODO: better job of transforming error for user consumption
+//       this.log(`${operation} failed: ${error.message}`);
+
+//       // Let the app keep running by returning an empty result.
+//       return of(result as T);
+//     };
+//   }
+
+//   private log(message: string) {
+//     console.log(`Cargas Service: ${message}`);
+//   }
+// }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable, of } from 'rxjs';
+// import { catchError, map, tap } from 'rxjs/operators';
+// import { CONFIG } from 'src/assets/config';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class LoginService {
 
-  constructor() { }
-}
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class LoginService {
+//   loginURL: string = CONFIG.loginURL;
+
+//   constructor(private http: HttpClient) {}
+
+//   login(user: User): Observable<any> {
+//     console.log(user);
+
+//     return this.http.post<any>(this.loginURL, user).pipe(
+//       tap(() => {
+//         this.log(`user ${user.username} authenticated!`);
+//       }),
+
+//       catchError(this.handleError<any>('loginUser'))
+//     );
+//   }
+
+//   private handleError<T>(operation = 'operation', result?: T) {
+//     return (error: any): Observable<T> => {
+//       this.log(error);
+//       this.log(`${operation} failed: ${error.message}`);
+//       return of(result as T);
+//     };
+//   }
+//   private log(message: string) {
+//     console.log(`Login Service: ${message}`);
+//   }
+// }
