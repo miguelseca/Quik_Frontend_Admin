0ddad6ae05454af4042b3b0acfa95e5b90f54220
Miguel Seca <miguel.seca@gmail.com>
2022-09-01 13:40:20 +0100

    login service implemented

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import User from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+import { CONFIG } from 'src/assets/config';
 
 @Component({
   selector: 'app-login-page',
@@ -8,68 +14,61 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class LoginPageComponent implements OnInit {
-  
 
-  constructor() {
-    
-   }
 
-  ngOnInit(): void {
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private snackBar: MatSnackBar
+    ) {}
+
+
+    loginForm: any;
+
+    ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      password: new FormControl('',[Validators.required, Validators.minLength(4)])
+    });
   }
+
+    onLogOnClick() {
+      const theUser: User = {
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password,
+
+      };
+
+
+
+
+      this.loginService.login(theUser).subscribe((data) => {
+        console.log("data received" + data.role);
+
+        localStorage.setItem('token', JSON.stringify(data));
+        console.log('token provided: ' + data.token);
+        if (!!data)
+          this.router.navigateByUrl('/dashboard');
+        if (!data) this.logSnacks("Autenticação falhou.", 2000);
+      });
+    }
+
+
+    // get username() {
+    //   return this.loginForm.get('username');
+    // }
+
+
+    logSnacks(message: string, time: number): void {
+      this.snackBar.open(message, '', { panelClass: 'snacks', duration: time });
+    }
 
 }
 
-// import { Component, OnInit } from '@angular/core';
-// import { FormControl, FormGroup, Validators } from '@angular/forms';
-// import { MatSnackBar } from '@angular/material/snack-bar';
-// import { Router } from '@angular/router';
-// import { User } from 'src/app/models/User';
-// import { LoginService } from 'src/app/services/login.service';
-// import { CONFIG } from 'src/assets/config';
 
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.css'],
-// })
-// export class LoginComponent implements OnInit {
-//   confirmButtonText: string = CONFIG.confirmButtonText;
-//   cancelButtonText: string = CONFIG.cancelButtonText;
-//   loginForm: any;
 
-//   constructor(
-//     private loginService: LoginService,
-//     private router: Router,
-//     private snackBar: MatSnackBar
-//   ) {}
 
-//   ngOnInit(): void {
-//     this.loginForm = new FormGroup({
-//       username: new FormControl('', [Validators.required, Validators.minLength(4)]),
-//       password: new FormControl('',[Validators.required, Validators.minLength(4)])
-//     });
-//   }
 
-//   onLogOnClick() {
-//     const theUser: User = {
-//       username: this.loginForm.value.username,
-//       password: this.loginForm.value.password,
-//     };
 
-//     this.loginService.login(theUser).subscribe((data) => {
-//       localStorage.setItem('token', JSON.stringify(data));
-//       console.log('token provided: ' + data);
-//       if (!!data)
-//         this.logSnacks(${theUser.username} autenticado com sucesso!, 1000);
-//       if (!data) this.logSnacks(Autenticação falhou., 5000);
-//     });
-//   }
 
-//   onCancelClick(): void {
-//     this.router.navigateByUrl('/voos');
-//   }
-
-//   get username() {
-//     return this.loginForm.get('username');
-//   }
 
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,38 +1,39 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { Observable, of } from 'rxjs';
-// import { catchError, map, tap } from 'rxjs/operators';
-// import { CONFIG } from 'src/assets/config';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map, share, tap } from 'rxjs/operators';
+import { CONFIG } from 'src/assets/config';
+import User from '../models/user';
 
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  loginURL: string = CONFIG.loginURL;
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class LoginService {
-//   loginURL: string = CONFIG.loginURL;
+  constructor(private http: HttpClient) {}
 
-//   constructor(private http: HttpClient) {}
+  login(user: User): Observable<any> {
+    console.log(user);
 
-//   login(user: User): Observable<any> {
-//     console.log(user);
+    return this.http.post<any>(this.loginURL, user).pipe(
+      tap(() => {
+        this.log(`user ${user.username} authenticated!`);
+      }),
 
-//     return this.http.post<any>(this.loginURL, user).pipe(
-//       tap(() => {
-//         this.log(`user ${user.username} authenticated!`);
-//       }),
+      catchError(this.handleError<any>('loginUser'))
+    );
 
-//       catchError(this.handleError<any>('loginUser'))
-//     );
-//   }
+  }
 
-//   private handleError<T>(operation = 'operation', result?: T) {
-//     return (error: any): Observable<T> => {
-//       this.log(error);
-//       this.log(`${operation} failed: ${error.message}`);
-//       return of(result as T);
-//     };
-//   }
-//   private log(message: string) {
-//     console.log(`Login Service: ${message}`);
-//   }
-// }
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      this.log(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+  private log(message: string) {
+    console.log(`Login Service: ${message}`);
+  }
+}
