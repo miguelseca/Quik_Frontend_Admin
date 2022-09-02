0feba023cc58cacc5fc88e10e1db5dbbc27305bd
Miguel Seca <miguel.seca@gmail.com>
2022-09-02 11:22:11 +0100

    interceptor implemented

--- src/app/interceptors/interceptor.ts
+++ src/app/interceptors/interceptor.ts
@@ -1,23 +1,23 @@
-// import { Injectable } from '@angular/core';
-// import {
-//   HttpInterceptor,
-//   HttpEvent,
-//   HttpResponse,
-//   HttpRequest,
-//   HttpHandler,
-// } from '@angular/common/http';
-// import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpResponse,
+  HttpRequest,
+  HttpHandler,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-// @Injectable()
-// export class Interceptor implements HttpInterceptor {
-//   constructor() {}
+@Injectable()
+export class Interceptor implements HttpInterceptor {
+  constructor() {}
 
-//   intercept(
-//     httpRequest: HttpRequest<any>,
-//     next: HttpHandler
-//   ): Observable<HttpEvent<any>> {
-//     const API_KEY = '123456';
-//     //return next.handle(httpRequest.clone({ setHeaders: { API_KEY } }));
-//     return next.handle(httpRequest);
-//   }
-// }
+  intercept(
+    httpRequest: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const API_KEY = '123456';
+    return next.handle(httpRequest.clone({ setHeaders: { API_KEY } }));
+    //return next.handle(httpRequest);
+  }
+}
