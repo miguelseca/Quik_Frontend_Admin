5a270b75330f6b38f8b256f4bf184bb95744198a
Miguel Seca <miguel.seca@gmail.com>
2022-09-07 12:11:53 +0100

    url updated

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -7,12 +7,22 @@ export const CONFIG = {
 
 
 
-    //================= URL's ===========================
-    DRIVERS_URL: 'http://127.0.0.1:1000/api/driver',
-    TRIPS_URL: 'http://127.0.0.1:1000/api/getAllTrips',
-    LOGIN_URL: 'http://127.0.0.1:1000/api/login',
-    ISSUES_URL: 'http://127.0.0.1:1000/api/issues',
-    GET_CLIENTS_URL: 'http://127.0.0.1:1000/api/clients',
-    BAN_CLIENTS_URL: 'http://127.0.0.1:1000/api/ban/client/',
+    //================= LOCAL URL's ===========================
+    // DRIVERS_URL: 'http://127.0.0.1:1000/api/driver',
+    // TRIPS_URL: 'http://127.0.0.1:1000/api/getAllTrips',
+    // LOGIN_URL: 'http://127.0.0.1:1000/api/login',
+    // ISSUES_URL: 'http://127.0.0.1:1000/api/issues',
+    // GET_CLIENTS_URL: 'http://127.0.0.1:1000/api/clients',
+    // BAN_CLIENTS_URL: 'http://127.0.0.1:1000/api/ban/client/',
+
+     //================= HEROKU URL's ===========================
+    DRIVERS_URL: 'https://quikbackend.herokuapp.com/api/driver',
+    TRIPS_URL: 'hhttps://quikbackend.herokuapp.com//api/getAllTrips',
+    LOGIN_URL: 'https://quikbackend.herokuapp.com/api/login',
+    ISSUES_URL: 'https://quikbackend.herokuapp.com/api/issues',
+    GET_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/clients',
+    BAN_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/ban/client/',
+
+
 
   };
