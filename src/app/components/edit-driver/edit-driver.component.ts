4865946c2df00dd37731c9d5c23cdebf4ee1cc63
Diogo Mendes <diogomendes@upskill.pt>
2022-09-07 14:45:16 +0100

    Edit driver implemented

--- src/app/components/edit-driver/edit-driver.component.ts
+++ src/app/components/edit-driver/edit-driver.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import Driver from 'src/app/models/driver';
+import Driver, { Service } from 'src/app/models/driver';
 import { DriversService } from 'src/app/services/drivers.service';
 
 @Component({
@@ -30,24 +30,41 @@ export class EditDriverComponent implements OnInit {
 
 
     this.editForm = new FormGroup({
-      firstName: new FormControl(this.driver.firstName, [Validators.required, Validators.minLength(3), Validators.maxLength(3)]),
-      lastName: new FormControl(this.driver.lastName, [Validators.required,  Validators.minLength(3), Validators.maxLength(3)]),
+      firstName: new FormControl(this.driver.firstName, [Validators.required,]),
+      lastName: new FormControl(this.driver.lastName, [Validators.required,]),
+      phone: new FormControl(this.driver.phone, [Validators.required]),
+      // email: new FormControl(this.driver.email, [Validators.required]),
+      code: new FormControl(this.driver.service.code, [Validators.required]),
+      carBrand: new FormControl(this.driver.service.carBrand, [Validators.required]),
+      carModel: new FormControl(this.driver.service.carModel, [Validators.required]),
+      licensePlate: new FormControl(this.driver.service.licensePlate, [Validators.required]),
       shift: new FormControl(this.driver.shift, [Validators.required]),
+     
+
     });
   }
 
   onConfirmClick(): void {
     if (this.editForm.valid) {
+      console.log("0: "+ JSON.stringify(this.editForm.value))
+    const x : Service ={
+      code:this.editForm.value.code,
+      carBrand:this.editForm.value.carBrand,
+      carModel:this.editForm.value.carModel,
+      licensePlate:this.editForm.value.licensePlate
+    
+    }
     const umDriver: Driver = {
       nif: this.driver.nif,
-      phone: this.driver.phone,
+      phone: this.editForm.value.phone,
       email: this.driver.email,
-      service: this.driver.service,
+      service:x,
       firstName: this.editForm.value.firstName,
       lastName: this.editForm.value.lastName,
       shift: this.editForm.value.shift,
 
     };
+    console.log("1: "+ JSON.stringify(umDriver))
 
     this.driverService.updateDriver(umDriver).subscribe((data) => {
       this.dialogRef.close(umDriver);
