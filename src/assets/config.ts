1596c58c0f042ca8a7b406988b7733b14f1aeb93
Diogo Mendes <diogomendes@upskill.pt>
2022-09-08 15:23:27 +0100

    css app + config code review +dashboard css

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,28 +1,22 @@
 export const CONFIG = {
-    title: 'QUIK ADMIN SYSTEM',
+  title: 'QUIK ADMIN SYSTEM',
 
+  CONFIRM_BUTTON_TEXT: 'Confirm',
+  CANCEL_BUTTON_TEXT: 'Cancel',
 
-    CONFIRM_BUTTON_TEXT: "Confirm",
-    CANCEL_BUTTON_TEXT: "Cancel",
+  //================= LOCAL URL's ===========================
+  // DRIVERS_URL: 'http://127.0.0.1:1000/api/driver',
+  // TRIPS_URL: 'http://127.0.0.1:1000/api/getAllTrips',
+  // LOGIN_URL: 'http://127.0.0.1:1000/api/login',
+  // ISSUES_URL: 'http://127.0.0.1:1000/api/issues',
+  // GET_CLIENTS_URL: 'http://127.0.0.1:1000/api/clients',
+  // BAN_CLIENTS_URL: 'http://127.0.0.1:1000/api/ban/client/',
 
-
-
-    //================= LOCAL URL's ===========================
-    // DRIVERS_URL: 'http://127.0.0.1:1000/api/driver',
-    // TRIPS_URL: 'http://127.0.0.1:1000/api/getAllTrips',
-    // LOGIN_URL: 'http://127.0.0.1:1000/api/login',
-    // ISSUES_URL: 'http://127.0.0.1:1000/api/issues',
-    // GET_CLIENTS_URL: 'http://127.0.0.1:1000/api/clients',
-    // BAN_CLIENTS_URL: 'http://127.0.0.1:1000/api/ban/client/',
-
-     //================= HEROKU URL's ===========================
-    DRIVERS_URL: 'https://quikbackend.herokuapp.com/api/driver',
-    TRIPS_URL: 'hhttps://quikbackend.herokuapp.com//api/getAllTrips',
-    LOGIN_URL: 'https://quikbackend.herokuapp.com/api/login',
-    ISSUES_URL: 'https://quikbackend.herokuapp.com/api/issues',
-    GET_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/clients',
-    BAN_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/ban/client/',
-
-
-
-  };
+  //================= HEROKU URL's ===========================
+  DRIVERS_URL: 'https://quikbackend.herokuapp.com/api/driver',
+  TRIPS_URL: 'https://quikbackend.herokuapp.com//api/getAllTrips',
+  LOGIN_URL: 'https://quikbackend.herokuapp.com/api/login',
+  ISSUES_URL: 'https://quikbackend.herokuapp.com/api/issues',
+  GET_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/clients',
+  BAN_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/ban/client/',
+};
