2dc2803cc215101c6fab12dd026bd51ff1005ee1
Rui Pinto <rfcapinto@gmail.com>
2022-09-09 10:26:24 +0100

    changes to app-routing, app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,74 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ClientsComponent } from './components/clients/clients.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DriversComponent } from './components/drivers/drivers.component';
 import { IssuesComponent } from './components/issues/issues.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
+// import { LoginPageComponent } from './components/login-page/login-page.component';
 import { EditDriverComponent } from './components/edit-driver/edit-driver.component';
 import { NewDriverComponent } from './components/new-driver/new-driver.component';
 import { AuthGuardService } from './services/auth-guard.service';
+import { LoginAdminComponent } from './components/login/login.component';
+import { MapsComponent } from './components/maps/maps.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    component: LoginPageComponent,
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'chart-Users',
-    component: LoginPageComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'chart-Revenue',
-    component: LoginPageComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'chart-Drivers',
-    component: LoginPageComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'issues',
-    component: IssuesComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'clients',
-    component: ClientsComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'drivers',
-    component: DriversComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'profile',
-    component: LoginPageComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'newDriver',
-    component: NewDriverComponent,
-    canActivate: [AuthGuardService],
-  },
-  {
-    path: 'editDriver',
-    component: EditDriverComponent,
-    canActivate: [AuthGuardService],
-  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginAdminComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuardService] },
+  { path: 'clients', component: ClientsComponent, canActivate: [AuthGuardService] },
+  { path: 'drivers', component: DriversComponent, canActivate: [AuthGuardService] },
+  { path: 'newDriver', component: NewDriverComponent, canActivate: [AuthGuardService] },
+  { path: 'editDriver', component: EditDriverComponent, canActivate: [AuthGuardService] },
+  { path: 'issues', component: IssuesComponent, canActivate: [AuthGuardService] },
+  { path: 'maps', component: MapsComponent, canActivate: [AuthGuardService] },
 ];
 
 @NgModule({
