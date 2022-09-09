8e7a423a91f8a43ba8e94e59058c35150a279b90
Rui Pinto <rfcapinto@gmail.com>
2022-09-09 10:50:26 +0100

    fixed url

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,11 +1,8 @@
 export const CONFIG = {
     title: 'QUIK ADMIN SYSTEM',
 
-
     CONFIRM_BUTTON_TEXT: "Confirm",
     CANCEL_BUTTON_TEXT: "Cancel",
-
-
 
     //================= LOCAL URL's ===========================
     // DRIVERS_URL: 'http://127.0.0.1:1000/api/driver',
@@ -17,12 +14,10 @@ export const CONFIG = {
 
      //================= HEROKU URL's ===========================
     DRIVERS_URL: 'https://quikbackend.herokuapp.com/api/driver',
-    TRIPS_URL: 'hhttps://quikbackend.herokuapp.com//api/getAllTrips',
+    TRIPS_URL: 'https://quikbackend.herokuapp.com//api/getAllTrips',
     LOGIN_URL: 'https://quikbackend.herokuapp.com/api/login',
     ISSUES_URL: 'https://quikbackend.herokuapp.com/api/issues',
     GET_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/clients',
-    BAN_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/ban/client/',
-
-
+    BAN_CLIENTS_URL: 'https://quikbackend.herokuapp.com/api/ban/client',
 
   };
