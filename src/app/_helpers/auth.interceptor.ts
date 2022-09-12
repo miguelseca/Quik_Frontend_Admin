9ca745109ca14cbc79c710dbc70378b3ffb63bdd
Rui Pinto <rfcapinto@gmail.com>
2022-09-12 11:43:19 +0100

    Code cleanup - deleted unused login-page component - deleted old interceptor - renamed driver.ts - added bootstrap to index.html - cleaned style.css - auth.interceptor unused code

--- src/app/_helpers/auth.interceptor.ts
+++ src/app/_helpers/auth.interceptor.ts
@@ -5,26 +5,18 @@ import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http'
 import { TokenStorageService } from '../services/token-storage.service';
 import { Observable } from 'rxjs';
 
-const TOKEN_HEADER_KEY = 'Authorization';       // for Spring Boot back-end
-// const TOKEN_HEADER_KEY = 'x-access-token';   // for Node.js Express back-end
+const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private token: TokenStorageService) { }
+  constructor(private tokenStorageService: TokenStorageService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
-    const token = this.token.getToken();
-    // console.log(token);
+    const token = this.tokenStorageService.getToken();
     
     if (token != null) {
-      // for Spring Boot back-end
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
-
-      // for Node.js Express back-end
-      // authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
-      console.log(authReq);
-      
     }
     return next.handle(authReq);
   }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { ClientsComponent } from './components/clients/clients.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DriversComponent } from './components/drivers/drivers.component';
 import { IssuesComponent } from './components/issues/issues.component';
-// import { LoginPageComponent } from './components/login-page/login-page.component';
 import { EditDriverComponent } from './components/edit-driver/edit-driver.component';
 import { NewDriverComponent } from './components/new-driver/new-driver.component';
 import { AuthGuardService } from './services/auth-guard.service';
